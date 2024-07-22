ff6538c626166d3ad3555278fab8fd3ca1633698
MinhoKK <koho1047@naver.com>
2024-07-23 01:21:58 +0900

    feat : #11 한줄자기소개 Redis 레포지토리 생성

---
+++ src/main/java/com/ssafy/freezetag/domain/result/repository/IntroRedisRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.freezetag.domain.result.repository;
+
+import com.ssafy.freezetag.domain.result.entity.IntroRedis;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IntroRedisRepository extends CrudRepository<IntroRedis, Long> {
+
+}
