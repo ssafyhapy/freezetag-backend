556b5440c61720ac0a529e4dee11dc1284482a7d
MinhoKK <koho1047@naver.com>
2024-07-23 16:34:50 +0900

    feat : #16 한줄자기소개 DTO 생성

---
+++ src/main/java/com/ssafy/freezetag/domain/result/service/request/IntroModifyRequestDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.freezetag.domain.result.service.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class IntroModifyRequestDto {
+    private final String id;    // redis 식별키
+
+    private final String content;
+}
