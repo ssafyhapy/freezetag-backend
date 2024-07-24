155e8b993052f7d4bb2373ae01eafa82ff68e04c
chestnut1717 <psh56750855@gmail.com>
2024-07-25 02:53:11 +0900

    feat : #15 로그아웃 기능 구현

--- src/main/java/com/ssafy/freezetag/global/config/SecurityConfig.java
+++ src/main/java/com/ssafy/freezetag/global/config/SecurityConfig.java
@@ -4,16 +4,24 @@ import com.ssafy.freezetag.domain.oauth2.TokenExceptionFilter;
 import com.ssafy.freezetag.domain.oauth2.handler.OAuth2SuccessHandler;
 import com.ssafy.freezetag.domain.oauth2.service.CustomOAuth2UserService;
 import com.ssafy.freezetag.domain.oauth2.service.TokenAuthenticationFilter;
+import com.ssafy.freezetag.domain.oauth2.service.TokenService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Configuration
@@ -24,8 +32,8 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
     private final TokenAuthenticationFilter tokenAuthenticationFilter;
+    private final TokenService tokenService; // 추가
 
-    // TODO : 나중에 전역 Pattern 지우기
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -33,19 +41,34 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions().disable())
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/", "/home", "/login", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/public/**").permitAll() // added "/public/**"
+                                .requestMatchers("/", "/home", "/login", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile", "/public/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2Login ->
                         oauth2Login
                                 .userInfoEndpoint(userInfoEndpoint ->
-                                                userInfoEndpoint.userService(customOAuth2UserService)
-                                        // 로그인 성공 시 핸들러
+                                        userInfoEndpoint.userService(customOAuth2UserService)
                                 )
                                 .successHandler(oAuth2SuccessHandler)
                 )
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/logout") // 로그아웃 요청 URL
+                                .logoutSuccessUrl("/")
+                                .invalidateHttpSession(true)
+                                .deleteCookies("JSESSIONID")
+                                .addLogoutHandler(new LogoutHandler() { // 익명 클래스
+                                    @Override
+                                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                        if (authentication != null) {
+                                            String username = authentication.getName();
+                                            tokenService.deleteRefreshToken(username); // Redis에서 토큰 삭제
+                                        }
+                                    }
+                                })
+                )
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(new TokenExceptionFilter(), tokenAuthenticationFilter.getClass()) // 토큰 예외 핸들링
+                .addFilterBefore(new TokenExceptionFilter(), tokenAuthenticationFilter.getClass())
         ;
 
         return http.build();
