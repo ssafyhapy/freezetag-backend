36ee7de03aa8f39e8673ee87fe95f716338b4c73
손지석 <thswltjr11@gmail.com>
2024-07-25 17:17:17 +0900

    feat/#39/openvidu_token_api_구현
    RoomController 방 생성 로직 구현

---
+++ src/main/java/com/ssafy/freezetag/domain/room/service/request/RoomCreateRequestDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.freezetag.domain.room.service.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RoomCreateRequestDto {
+    private final String roomName;
+    private final String roomCode;
+    private final Integer roomPersonCount;
+}
