9477a85ff8a75dcabe24aa00149b4fa9d8322cff
chestnut1717 <psh56750855@gmail.com>
2024-07-27 23:39:16 +0900

    feat: #15 httpOnly 쿠키로 accessToken 생성
    유효시간 : 1시간

--- src/main/java/com/ssafy/freezetag/domain/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/ssafy/freezetag/domain/oauth2/handler/OAuth2SuccessHandler.java
@@ -2,6 +2,7 @@ package com.ssafy.freezetag.domain.oauth2.handler;
 
 import com.ssafy.freezetag.domain.oauth2.TokenProvider;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,13 +24,21 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException, ServletException {
         // accessToken, refreshToken 발급
         String accessToken = tokenProvider.generateAccessToken(authentication);
-        tokenProvider.generateRefreshToken(authentication, accessToken);
+        // refreshToken 생성 후 redis에 저장
+        tokenProvider.generateRefreshToken(authentication);
+
+        // HTTP-only 쿠키로 accessToken 설정
+        Cookie cookie = new Cookie("accessToken", accessToken);
+        cookie.setHttpOnly(true); // 쿠키를 HTTP-only로 설정
+//        cookie.setSecure(true); // HTTPS에서만 쿠키를 전송하도록 설정
+        cookie.setPath("/"); // 쿠키의 유효 범위를 설정
+        cookie.setMaxAge(3600); // 쿠키의 유효 기간 설정 (지금은 1시간)
+
+        // 쿠키를 응답에 추가
+        response.addCookie(cookie);
 
         // 토큰 전달을 위한 redirect
         String redirectUrl = UriComponentsBuilder.fromUriString(URI).build().toUriString();
-//                .queryParam("accessToken", accessToken)
-//                .build().toUriString();
-        System.out.println(accessToken);
         response.sendRedirect(redirectUrl);
     }
 }
