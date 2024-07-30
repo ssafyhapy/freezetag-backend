98920b89738dee545ba1a55ea15185f18b214c90
chestnut1717 <psh56750855@gmail.com>
2024-07-31 02:18:11 +0900

    refactor: #53 레디스에서 refreshToken 삭제되도록 수정
    memberId를 Long => String으로 type변환

--- src/main/java/com/ssafy/freezetag/domain/member/service/MemberService.java
+++ src/main/java/com/ssafy/freezetag/domain/member/service/MemberService.java
@@ -2,15 +2,11 @@ package com.ssafy.freezetag.domain.member.service;
 
 import com.ssafy.freezetag.domain.exception.custom.TokenException;
 import com.ssafy.freezetag.domain.oauth2.TokenProvider;
-import com.ssafy.freezetag.domain.oauth2.entity.Token;
 import com.ssafy.freezetag.domain.oauth2.service.TokenService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -56,35 +52,25 @@ public class MemberService {
 
         // accessToken 조회
         String accessToken = request.getHeader("Authorization");
-//
-//
-//        // 인증 정보 확인
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null) {
-            throw new RuntimeException("현재 사용자의 인증 정보 오류가 발생하였습니다.");
-        }
 
-        String memberId = authentication.getName();
+
+
         // 그리고 access, refresh간 id 불일치 발생하면 처리
         if(!tokenProvider.validateSameTokens(accessToken, refreshToken)) {
             throw new TokenException("Access Token과 Refresh Token 사용자 정보 불일치합니다.");
         }
 
-        // 현재 시큐리티 세션의 id랑 accessToken 불일치 발생하면 오류
-        if(!tokenProvider.getAuthentication((accessToken)).getName().equals(memberId)) {
-            throw new TokenException("유저와 사용자 정보가 불일치합니다.");
-        }
+        Long memberId = tokenProvider.getMemberIdFromToken(refreshToken);
+        System.out.println(memberId);
         // 레디스에서 정보 삭제
-        tokenService.deleteRefreshToken(memberId);
+        tokenService.deleteRefreshToken(memberId.toString());
 
     }
 
     /*
         레디스의 refreshToken, 세션 정보 삭제
      */
-    public void deleteTokenAndSession(HttpServletRequest request, HttpServletResponse response) {
-        // 로그아웃 후 세션 무효화 및 쿠키 삭제
-        request.getSession().invalidate();
+    public void deleteToken(HttpServletRequest request, HttpServletResponse response) {
 
         // refreshToken 쿠키 삭제
         Cookie refreshTokenCookie = new Cookie("refreshToken", null);
@@ -92,11 +78,6 @@ public class MemberService {
         refreshTokenCookie.setPath("/"); // 쿠키가 유효한 경로 설정
         response.addCookie(refreshTokenCookie);
 
-        // 세션 쿠키 삭제
-        Cookie sessionCookie = new Cookie("JSESSIONID", null);
-        sessionCookie.setPath("/"); // 쿠키의 유효 경로 설정
-        sessionCookie.setMaxAge(0); // 쿠키의 유효 기간을 0으로 설정하여 삭제
-        response.addCookie(sessionCookie);
     }
 
     /*
