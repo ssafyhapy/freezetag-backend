e8b3f18dac1170ce98440f588f68840d7c20a9d5
MinhoKK <koho1047@naver.com>
2024-07-30 17:27:57 +0900

    feat : #64 Swagger config 설정

---
+++ src/main/java/com/ssafy/freezetag/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.ssafy.freezetag.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Info info = new Info()
+                .version("1.0.0")
+                .title("사르르")
+                .description("사르르 API 문서입니다.");
+
+        String jwtSchemeName = "JwtAuthorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addServersItem(new Server().url("/"));
+    }
+}
