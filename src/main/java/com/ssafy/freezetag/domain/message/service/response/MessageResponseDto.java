76a0d1204078102959a06202cbc9367222341fb0
MinhoKK <koho1047@naver.com>
2024-08-02 23:54:52 +0900

    refactor : #113 @RequiredArgsConstructor 생성자 주입 리팩토링

--- src/main/java/com/ssafy/freezetag/domain/message/service/response/MessageResponseDto.java
+++ src/main/java/com/ssafy/freezetag/domain/message/service/response/MessageResponseDto.java
@@ -1,20 +1,16 @@
 package com.ssafy.freezetag.domain.message.service.response;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@RequiredArgsConstructor
 public class MessageResponseDto {
     private final String memberName;
 
     private final String content;
 
     private final LocalDateTime time;
-
-    public MessageResponseDto(String memberName, String content, LocalDateTime time) {
-        this.memberName = memberName;
-        this.content = content;
-        this.time = time;
-    }
 }
