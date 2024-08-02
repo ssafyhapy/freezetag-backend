56c3870332b5aade07416109484e2aaa22856757
MinhoKK <koho1047@naver.com>
2024-08-02 13:30:03 +0900

    feat : #67 사용하지 않는 어노테이션 제거

--- src/main/java/com/ssafy/freezetag/global/config/WebSocketConfig.java
+++ src/main/java/com/ssafy/freezetag/global/config/WebSocketConfig.java
@@ -1,7 +1,5 @@
 package com.ssafy.freezetag.global.config;
 
-import com.ssafy.freezetag.domain.oauth2.TokenProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -12,7 +10,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
-@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
