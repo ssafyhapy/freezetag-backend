af492fd9b474eeff7cb87313c8c67bb25570fa92
MinhoKK <koho1047@naver.com>
2024-08-02 13:14:41 +0900

    feat : #67 채팅 엔티티 생성

---
+++ src/main/java/com/ssafy/freezetag/domain/message/entity/MessageRedis.java
@@ -0,0 +1,33 @@
+package com.ssafy.freezetag.domain.message.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@RedisHash(value = "message")
+public class MessageRedis {
+    @Id
+    private String id;
+
+    @Indexed
+    private Long roomId;
+
+    private String memberName;
+
+    private String content;
+
+    private LocalDateTime createdDate;
+
+    public MessageRedis(Long roomId, String memberName, String content) {
+        this.id = UUID.randomUUID().toString();
+        this.roomId = roomId;
+        this.memberName = memberName;
+        this.content = content;
+        this.createdDate = LocalDateTime.now();
+    }
+}
