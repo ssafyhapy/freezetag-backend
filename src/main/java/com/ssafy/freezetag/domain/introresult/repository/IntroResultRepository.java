070dfc02a655acf61d15dcaf85db8da5eb166953
MinhoKK <koho1047@naver.com>
2024-08-05 02:05:00 +0900

    feat : #129 MemberRoomId로 한 줄 자기소개 찾기

--- src/main/java/com/ssafy/freezetag/domain/introresult/repository/IntroResultRepository.java
+++ src/main/java/com/ssafy/freezetag/domain/introresult/repository/IntroResultRepository.java
@@ -4,6 +4,9 @@ import com.ssafy.freezetag.domain.introresult.entity.IntroResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IntroResultRepository extends JpaRepository<IntroResult, Long> {
+    Optional<IntroResult> findByMemberRoomId(Long memberRoomId);
 }
