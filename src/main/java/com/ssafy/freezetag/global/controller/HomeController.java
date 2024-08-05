8084b3288f8ddfadaba93deaf6d933e63005a693
손지석 <thswltjr11@gmail.com>
2024-08-05 14:02:00 +0900

    feat : #143 s3 컨트롤러 구현

--- src/main/java/com/ssafy/freezetag/global/controller/HomeController.java
+++ src/main/java/com/ssafy/freezetag/global/controller/HomeController.java
@@ -1,24 +1,30 @@
 package com.ssafy.freezetag.global.controller;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import com.ssafy.freezetag.global.s3.S3UploadService;
+import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
-
-import java.util.Map;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HomeController {
+
+    private final S3UploadService s3UploadService;
+
+    public HomeController(final S3UploadService s3UploadService) {
+        this.s3UploadService = s3UploadService;
+    }
 
     @GetMapping("/")
     public String homePage(Model model) {
@@ -60,4 +66,15 @@ public class HomeController {
         // ResponseEntity의 모든 요소를 반환
         return new ResponseEntity<>(responseEntity.getBody(), responseEntity.getHeaders(), responseEntity.getStatusCode());
     }
+
+    @PostMapping(value = "/api/s3/test", consumes = "multipart/form-data")
+    public ResponseEntity<?> test(@RequestParam("file") MultipartFile file) {
+        try {
+            String url = s3UploadService.saveFile(file);
+            return ResponseEntity.ok(url);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File upload failed: " + e.getMessage());
+        }
+    }
+
 }
