55484a4fe7a9db08183f8da20a082d00b66ce8d3
손지석 <thswltjr11@gmail.com>
2024-08-05 15:24:53 +0900

    fix : #157 Secret Key env 수정

--- src/test/java/com/ssafy/freezetag/FreezetagApplicationTests.java
+++ src/test/java/com/ssafy/freezetag/FreezetagApplicationTests.java
@@ -1,38 +1,10 @@
 package com.ssafy.freezetag;
 
-import groovy.util.logging.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "openai.key=${OPEN_AI_KEY}",
-        "s3.access.key=${S3_ACCESS_KEY}",
-        "s3.secret.key=${S3_SECRET_KEY}"
-})
-@Slf4j
 public class FreezetagApplicationTests {
-    @Value("${openai.key}")
-    private String openAiKey;
 
-    @Value("${s3.access.key}")
-    private String s3AccessKey;
-
-    @Value("${s3.secret.key}")
-    private String s3SecretKey;
-
-    @Test
-    public void contextLoads() {
-        // 환경 변수가 올바르게 주입되었는지 확인하는 테스트 코드
-        System.out.println(openAiKey + " : " + s3AccessKey + " : " + s3SecretKey);
-        assertNotNull(openAiKey);
-        assertNotNull(s3AccessKey);
-        assertNotNull(s3SecretKey);
-    }
 }
 
 
