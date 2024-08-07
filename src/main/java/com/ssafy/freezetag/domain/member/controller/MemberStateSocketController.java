5adfa75d9aece6537d45588c2a9e5ed00bd5b5c5
MinhoKK <koho1047@naver.com>
2024-08-07 15:41:27 +0900

    refactor : #187 시용자 상태 로그 추가

--- src/main/java/com/ssafy/freezetag/domain/member/controller/MemberStateSocketController.java
+++ src/main/java/com/ssafy/freezetag/domain/member/controller/MemberStateSocketController.java
@@ -22,7 +22,9 @@ public class MemberStateSocketController {
     @MessageMapping("/{roomId}/state")
     public void getNowState(@DestinationVariable Long roomId, @Payload MemberStateSocketRequestDto memberStateSocketRequestDto) {
 
+        log.info("{}번 회원님이 {}상태로 넘어가도록 알림을 보냈습니다.", roomId, memberStateSocketRequestDto.getMemberState());
         simpMessageSendingOperations.convertAndSend("/api/sub" + roomId + "/state", memberStateSocketRequestDto);
+        log.info("{}번 회원님이 {}상태로 성공적으로 넘어갔습니다.", roomId, memberStateSocketRequestDto.getMemberState());
     }
 
 }
