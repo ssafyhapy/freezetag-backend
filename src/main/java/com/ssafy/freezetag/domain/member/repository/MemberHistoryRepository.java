bc0899fad8dc5a701063d298e7fb96a167cc65d7
chestnut1717 <psh56750855@gmail.com>
2024-08-11 18:16:33 +0900

    feat: #227 : member, MemberHistory  Fetch Join

--- src/main/java/com/ssafy/freezetag/domain/member/repository/MemberHistoryRepository.java
+++ src/main/java/com/ssafy/freezetag/domain/member/repository/MemberHistoryRepository.java
@@ -1,11 +1,27 @@
 package com.ssafy.freezetag.domain.member.repository;
 
+import com.ssafy.freezetag.domain.member.entity.Member;
 import com.ssafy.freezetag.domain.member.entity.MemberHistory;
 import java.util.List;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberHistoryRepository extends JpaRepository<MemberHistory, Long> {
     @EntityGraph(attributePaths = {"member"})
     List<MemberHistory> findByMemberId(Long memberId);
+
+    @Query("SELECT m FROM Member m " +
+            "LEFT JOIN FETCH m.memberHistories mh " +
+            "WHERE m.id = :memberId")
+    Member findMemberWithHistories(Long memberId);
+
+
+//    // history가 없을 수도 있으므로 RIGHT JOIN 해야 함
+//    @Query("SELECT mh " +
+//            "FROM MemberHistory mh " +
+//            "RIGHT JOIN FETCH mh.member m " +
+//            "WHERE m.id = :memberId")
+//    List<MemberHistory> findAllByMemberIdWithFetchJoinHistory(Long memberId);
+
 }
